8b0c43b54a8d3be62ba58ddd24a9678c8801e53c
Clayton Passmore <ctpassmore@gmail.com>
2018-08-16 00:12:19 -0400

    Remove react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
 // import logo from './logo.svg';
 import './App.css';
 import Resume from './components/Resume';
 
 class App extends Component {
   render() {
-    return (
-      <BrowserRouter>
-        <Route exact path="/" component={Resume}/>
-      </BrowserRouter>
-    );
+    return (<Resume/>);
   }
 }
 
