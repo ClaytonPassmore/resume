16a0c65576d6b98177ba1c5ae1167015aed323ab
Clayton Passmore <ctpassmore@gmail.com>
2018-08-29 21:39:11 -0400

    Add console easter egg

--- src/App.js
+++ src/App.js
@@ -3,6 +3,15 @@ import './App.css';
 import Resume from './components/Resume';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log(
+      "Wondering how this was made?\n\n" +
+      "Check it out here:\n" +
+      "https://github.com/ClaytonPassmore/resume"
+    );
+  }
+
   render() {
     return (<Resume/>);
   }
